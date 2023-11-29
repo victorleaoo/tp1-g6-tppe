e066f405acc267497d4ae46f32732f37ea4aae57
Vinicius Assumpção de Araujo <78980842+viniman27@users.noreply.github.com>
2023-11-29 10:31:06 -0300

    duplicaçãoPassa: Gestao de transacoes

--- GestaoDeTransacoes/GestaoTransacoes.py
+++ GestaoDeTransacoes/GestaoTransacoes.py
@@ -3,5 +3,7 @@ class ValorInvalidoException(Exception):
 
 class GerenciadorDeInventario:
     def registrar_venda(self, id_produto, quantidade):
-
+        # Verificando se a quantidade é negativa
+        if quantidade < 0:
+            raise ValorInvalidoException("Quantidade não pode ser negativa")        
         return True
