ef9b8f5518f7d875b6507f8f921a8aea450a8831
Vinicius Assumpção de Araujo <78980842+viniman27@users.noreply.github.com>
2023-11-29 10:37:54 -0300

    triangulaçãoFalha: Gestao de transacoes

--- GestaoDeTransacoes/testTransacoes.py
+++ GestaoDeTransacoes/testTransacoes.py
@@ -26,3 +26,16 @@ class TesteTransacoesDeInventario(unittest.TestCase):
 
         with self.assertRaises(ValorInvalidoException):
             gerenciador.registrar_venda(id_produto, quantidade_negativa)
+
+    def teste_registrar_transferencia_entre_filiais(self):
+        gerenciador = GerenciadorDeInventario()
+        id_produto = "12345"
+        quantidade = 5
+        id_filial_origem = "001"
+        id_filial_destino = "002"
+
+        # Tentativa de registrar uma transferência entre filiais
+        resultado = gerenciador.registrar_transferencia(id_filial_origem, id_filial_destino, id_produto, quantidade)
+
+        # Verificando se a transferência foi registrada corretamente
+        self.assertTrue(resultado)
