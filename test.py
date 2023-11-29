d50cf792d9591394c46a156428878eec28fe9af7
victorleaoo <victor.pessoal1203@gmail.com>
2023-11-28 22:48:16 -0300

    parametrizacao: descricao em branco (cadastro produto)

--- test.py
+++ test.py
@@ -20,14 +20,22 @@ def test_cadastro_produto():
     assert produto.lote == 0
     assert produto.data_validade == "12/01/2024"
 
-def test_produto_missing_arguments():
-    with pytest.raises(DescricaoEmBrancoException):
-        produto = Produto(
-            None, "123456789", 10.0, 20.0, 50, "1", "22/10/2026"
-        )
+test_data = [
+    (None, "123456789", 10.0, 20.0, 50, "11", "22/10/2026"),
+    ("Shampoo", None, 5.0, 10.0, 20, "22", "12/02/2030"),
+    ("Sabonete", "987654321", None, 1.0, 1, "33", "01/01/2024"),
+    ("Tenis", "123987456", 49.99, None, 500, "44", "09/06/2096"),
+    ("Computador", "000000000", 22.22, 157.55, None, "55", "07/07/2027"),
+    ("Camisa", "998888444", 59.10, 299.99, 11, None, "11/11/2111"),
+    ("Presunto", "123321213", 10.0, 20.0, 22, "66", None),
+]
 
-def test_produto_missing_arguments_dois():
+@pytest.mark.parametrize(
+    "nome, codigo_barras, preco_compra, preco_venda, quantidade_inicial, lote, data_validade",
+    test_data
+)
+def test_produto_missing_arguments(nome, codigo_barras, preco_compra, preco_venda, quantidade_inicial, lote, data_validade):
     with pytest.raises(DescricaoEmBrancoException):
         produto = Produto(
-            "Tenis Nike", None, 150.20, 400.0, 1, "2", "15/12/2022"
+            nome, codigo_barras, preco_compra, preco_venda, quantidade_inicial, lote, data_validade
         )
