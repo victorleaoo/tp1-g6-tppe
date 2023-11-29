d1ba99122bded23b8cf7ab948bab307e12624139
Vinicius Assumpção de Araujo <78980842+viniman27@users.noreply.github.com>
2023-11-29 11:14:56 -0300

    parametrizacao_teste: Gestao de transacoes

--- GestaoDeTransacoes/testTransacoes.py
+++ GestaoDeTransacoes/testTransacoes.py
@@ -1,41 +1,52 @@
-# test_inventario.py
+
 import unittest
 from GestaoTransacoes import GerenciadorDeInventario, ValorInvalidoException
 
 class TesteTransacoesDeInventario(unittest.TestCase):
 
-    def teste_registrar_transacao_venda(self):
-        # Criando uma instância do gerenciador de inventário
-        gerenciador = GerenciadorDeInventario()
+    def setUp(self):
+        self.gerenciador = GerenciadorDeInventario()
 
-        # Dados fictícios para a transação
+    def teste_transacoes_variadas(self):
+        # Dados de teste parametrizados
+        transacoes = [
+            ("recebimento", "12345", 20, "001"),
+            ("venda", "12345", 10, "001"),
+            ("transferencia", "001", "002", "12345", 5),
+            ("devolucao", "12345", 3, "001"),
+            ("ajuste", "12345", -2, "001")
+        ]
+
+        for transacao in transacoes:
+            with self.subTest(transacao=transacao):
+                tipo, *args = transacao
+                resultado = getattr(self.gerenciador, f"registrar_{tipo}")(*args)
+                self.assertTrue(resultado)
+
+                # Verificando se o estoque foi atualizado corretamente
+                if tipo != "transferencia":
+                    id_produto, _, id_filial = args if tipo != "ajuste" else (args[0], args[1], args[2])
+                    estoque_atual = self.gerenciador.consultar_estoque(id_produto, id_filial)
+                    self.assertIsNotNone(estoque_atual)
+
+    def teste_transacoes_quantidades_negativas(self):
+        # Dados de teste parametrizados
+        transacoes = [
+            ("venda", "12345", -10, "001"),
+            ("transferencia", "001", "002", "12345", -5),
+            ("devolucao", "12345", -3, "001")
+        ]
+
+        for transacao in transacoes:
+            with self.subTest(transacao=transacao):
+                tipo, *args = transacao
+                with self.assertRaises(ValorInvalidoException):
+                    getattr(self.gerenciador, f"registrar_{tipo}")(*args)
+
+    def teste_ajuste_estoque_negativo(self):
+        # Testando um ajuste que resultaria em estoque negativo
         id_produto = "12345"
-        quantidade = 10
-
-        # Tentativa de registrar uma transação de venda
-        resultado = gerenciador.registrar_venda(id_produto, quantidade)
-
-        # Verificando se a transação foi registrada corretamente
-        self.assertTrue(resultado)
-
-    def teste_registrar_venda_com_quantidade_negativa(self):
-            
-        gerenciador = GerenciadorDeInventario()
-        id_produto = "12345"
-        quantidade_negativa = -10
-
+        quantidade_ajuste_negativa = -100  # Supondo que isso exceda o estoque existente
+        id_filial = "001"
         with self.assertRaises(ValorInvalidoException):
-            gerenciador.registrar_venda(id_produto, quantidade_negativa)
-
-    def teste_registrar_transferencia_entre_filiais(self):
-        gerenciador = GerenciadorDeInventario()
-        id_produto = "12345"
-        quantidade = 5
-        id_filial_origem = "001"
-        id_filial_destino = "002"
-
-        # Tentativa de registrar uma transferência entre filiais
-        resultado = gerenciador.registrar_transferencia(id_filial_origem, id_filial_destino, id_produto, quantidade)
-
-        # Verificando se a transferência foi registrada corretamente
-        self.assertTrue(resultado)
+            self.gerenciador.registrar_ajuste(id_produto, quantidade_ajuste_negativa, id_filial)
