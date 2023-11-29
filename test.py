86ae442a9832b55dd8ab1236ff1ebfbe68200952
victorleaoo <victor.pessoal1203@gmail.com>
2023-11-28 22:05:05 -0300

    falsificaoFalha: descricao em branco (cadastro produto)

---
+++ test.py
@@ -0,0 +1,19 @@
+import pytest
+
+def test_cadastro_produto():
+    produto = Produto(
+        nome="Shampoo Suave",
+        codigo_barras="123456789",
+        preco_compra=5.50,
+        preco_venda=15.20,
+        quantidade_inicial=10,
+        lote=0,
+        data_validade="12/01/2024")
+
+    assert produto.nome == "Shampoo Suave"
+    assert produto.codigo_barras == "123456789"
+    assert produto.preco_compra == 10.0
+    assert produto.preco_venda == 20.0
+    assert produto.quantidade_estoque == 50
+    assert produto.lote == 0
+    assert produto.data_validade == "12/01/2024"
