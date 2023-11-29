704cdee4926718c25c42fa9b893573db318c5297
Vinicius Assumpção de Araujo <78980842+viniman27@users.noreply.github.com>
2023-11-29 10:15:16 -0300

    falsificacaoPassa: Gestao de transacoes

--- GestaoDeTransacoes/GestaoTransacoes.py
+++ GestaoDeTransacoes/GestaoTransacoes.py
@@ -1,7 +1,7 @@
-# inventario.py
 class ValorInvalidoException(Exception):
     pass
 
 class GerenciadorDeInventario:
     def registrar_venda(self, id_produto, quantidade):
-        pass
+
+        return True
