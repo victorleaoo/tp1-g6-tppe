657bbfe883115496a3a6d9002e49fc10cb54359a
Pedro <190094486@aluno.unb.br>
2023-11-30 13:42:10 -0300

    triangulaçãoFalha: consulta estoque

--- testConsultaEstoque.py
+++ testConsultaEstoque.py
@@ -10,3 +10,9 @@ def test_consulta_estoque_codigo():
     produto = Produto("123456789", 10.0, 15.0, 10, "Lote A", "2023-12-31","Produto B")
     resultado_consulta = produto.consulta_estoque_codigo(produto.codigo_barras)
     assert resultado_consulta == produto 
+
+#O produto pode ter o mesmo nome, mas não o mesmo codigo.
+def test_consulta_estoque():
+    produto = Produto("123456789", 10.0, 15.0, 10, "Lote A", "2023-12-31","Produto B")
+    resultado_consulta = produto.consulta_estoque(produto.nome,produto.codigo_barras)
+    assert resultado_consulta == produto 
