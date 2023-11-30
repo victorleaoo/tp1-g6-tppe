a0695b98b979c90c6a3ac78ea53c10f51035b97a
Iago <iagocabral.software@gmail.com>
2023-11-29 23:00:53 -0300

    parametrizacao: verifica validade

--- teste_validade.py
+++ teste_validade.py
@@ -16,7 +16,7 @@ def test_produto_fora_validade():
     )
     assert produto.esta_no_periodo_de_validade() == False
 
-def test_produto_na_validade():
+def test_data_vazia():
     with pytest.raises(DescricaoEmBrancoException):
         produto = Produto(
             nome="Shampoo na validade",
@@ -28,3 +28,26 @@ def test_produto_na_validade():
             data_validade=""
         )
     assert "produto" not in locals()
+
+@pytest.mark.parametrize("nome, codigo_barras, preco_compra, preco_venda, quantidade_inicial, lote, data_validade, esperado", [
+    ("Produto A", "1234567890", 10.0, 15.0, 5, "Lote A", "15/03/2001", False),
+    ("Produto B", "1111111111", 50.0, 70.0, 25, "Lote F", "15/04/2025", True),
+    ("Produto C", "2222222222", 50.0, 70.0, 25, "Lote F", "17/06/1999", False),
+    ("Produto D", "3333333333", 50.0, 70.0, 25, "Lote F", "17/06/2024", True),
+    ("Produto E", "4444444444", 50.0, 70.0, 25, "Lote F", "01/01/2025", True),
+    ("Produto F", "5555555555", 50.0, 70.0, 25, "Lote F", "07/02/1966", False),
+    ("Produto G", "6666666666", 50.0, 70.0, 25, "Lote F", "23/12/2029", True),
+    ("Produto H", "7777777777", 50.0, 70.0, 25, "Lote F", "01/01/2001", False),
+])
+def test_produto_validade(nome, codigo_barras, preco_compra, preco_venda, quantidade_inicial, lote, data_validade, esperado):
+    produto = Produto(
+        nome=nome,
+        codigo_barras=codigo_barras,
+        preco_compra=preco_compra,
+        preco_venda=preco_venda,
+        quantidade_inicial=quantidade_inicial,
+        lote=lote,
+        data_validade=data_validade
+    )
+    assert produto.esta_no_periodo_de_validade() == esperado
+    
