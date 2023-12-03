b92b1f6a803a00fd0f55040d16d41e8f28852ddc
João Pedro de Camargo Vaz <joaopedrocvaz@gmail.com>
2023-12-03 15:35:42 -0300

    teste para verificar se id pode ser string
    teste para verificar se id pode ser string

--- test_fornecedor.py
+++ test_fornecedor.py
@@ -16,6 +16,10 @@ class TestFornecedor(unittest.TestCase):
     def test_criar_fornecedor_com_id_invalido_vazio(self):
         with self.assertRaises(ValueError):
             Fornecedor('', "Fornecedor C", "Endereço C", "2023-01-01")
+
+    def test_criar_fornecedor_id_string(self):
+        with self.assertRaises(ValueError):
+            Fornecedor("abc", "Fornecedor D", "Endereço D", "2023-01-01")
         
 if __name__ == '__main__':
     unittest.main()
