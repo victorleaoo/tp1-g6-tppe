4f3d7ab1762e22b3d02379cd29e85400280493b6
João Pedro de Camargo Vaz <joaopedrocvaz@gmail.com>
2023-12-03 17:17:41 -0300

    adiciona trativa para a data
    adiciona trativa para a data

--- main/fornecedor.py
+++ main/fornecedor.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+
 class Fornecedor:
     def __init__(self, id, nome, endereco, data_contrato):
         self.validar_id(id)
         self.validar_nome(nome)
         self.validar_endereco(endereco)
+        self.validar_data_contrato(data_contrato)
         self.id = id
         self.nome = nome
         self.endereco = endereco
@@ -24,3 +27,12 @@ class Fornecedor:
     def validar_endereco(endereco):
         if endereco is None or endereco == '':
             raise ValueError("Endereço não pode ser nulo ou vazio")
+
+    @staticmethod
+    def validar_data_contrato(data_contrato):
+        try:
+            datetime.strptime(data_contrato, '%Y-%m-%d')
+        except ValueError:
+            raise ValueError("Data de contrato inválida, formato esperado: AAAA-MM-DD")
+
+        
