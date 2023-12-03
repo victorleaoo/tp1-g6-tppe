c93c6a10186b6a8bd702a358e1153c11554cf096
João Pedro de Camargo Vaz <joaopedrocvaz@gmail.com>
2023-12-03 15:13:56 -0300

    Adiciona tratativa para o id
    implementa o id para passar no teste de caso o id seja nulo

--- main/fornecedor.py
+++ main/fornecedor.py
@@ -1,6 +1,12 @@
 class Fornecedor:
     def __init__(self, id, nome, endereco, data_contrato):
+        self.validar_id(id)
         self.id = id
         self.nome = nome
         self.endereco = endereco
         self.data_contrato = data_contrato
+    
+    @staticmethod
+    def validar_id(id):
+        if id is None:
+            raise ValueError("ID não pode ser nulo")
