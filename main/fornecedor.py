969ae9331e652d42b81ab89f4fa8d3d00fa273b7
João Pedro de Camargo Vaz <joaopedrocvaz@gmail.com>
2023-12-03 17:07:23 -0300

    adiciona metodo para validar o nome
    adiciona metodo para validar o nome

--- main/fornecedor.py
+++ main/fornecedor.py
@@ -1,6 +1,7 @@
 class Fornecedor:
     def __init__(self, id, nome, endereco, data_contrato):
         self.validar_id(id)
+        self.validar_nome(nome)
         self.id = id
         self.nome = nome
         self.endereco = endereco
@@ -8,7 +9,12 @@ class Fornecedor:
     
     @staticmethod
     def validar_id(id):
-        if id is None or id is '':
+        if id is None or id == '':
             raise ValueError("ID não pode ser nulo")
         if isinstance(id, str):
             raise ValueError("ID não pode ser uma string")
+
+    @staticmethod
+    def validar_nome(nome):
+        if nome is None or nome == '':
+            raise ValueError("Nome não pode ser nulo ou vazio")
