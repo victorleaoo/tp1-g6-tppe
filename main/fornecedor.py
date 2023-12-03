50ba3990c90bf6ee8b9b4e1911d5d3c56beb1a73
João Pedro de Camargo Vaz <joaopedrocvaz@gmail.com>
2023-12-03 14:15:16 -0300

    definicao inicial da classe fornecedor
    definicao inicial da classe fornecedor

---
+++ main/fornecedor.py
@@ -0,0 +1,6 @@
+class Fornecedor:
+    def __init__(self, id, nome, endereco, data_contrato):
+        self.id = id
+        self.nome = nome
+        self.endereco = endereco
+        self.data_contrato = data_contrato
